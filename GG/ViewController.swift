aa6e4875f0e7a1f77f2fb50062bcf84de3bf6520
Felipe De La Torre <felipe9141@sbcglobal.net>
2018-11-09 17:32:17 -0800

    Initial Commit

---
+++ GG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GG
+//
+//  Created by Felipe De La Torre on 11/9/18.
+//  Copyright © 2018 Felipe De La Torre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ GGTests/GGTests.swift
@@ -0,0 +1,34 @@
+//
+//  GGTests.swift
+//  GGTests
+//
+//  Created by Felipe De La Torre on 11/9/18.
+//  Copyright © 2018 Felipe De La Torre. All rights reserved.
+//
+
+import XCTest
+@testable import GG
+
+class GGTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
